aabbfe78afea4fa879fe8f087db0d64d71b0b65d
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-21 00:00:33 +0600

    update controller

--- src/main/java/com/shahariyr/mvc/controller/MainController.java
+++ src/main/java/com/shahariyr/mvc/controller/MainController.java
@@ -1,14 +1,20 @@
 package com.shahariyr.mvc.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shahariyr.mvc.entity.customer;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/hello")
-	public String sayhello() {
-
+	@GetMapping("/")
+	public String sayhello(Model theModel) {
+		theModel.addAttribute("customer",new customer());
+		
+		
 		return "Index";
 	}
 }
