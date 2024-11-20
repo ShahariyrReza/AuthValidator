9f91ff94965ee1e0945bf1b9e73cb8aebda62637
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-20 22:44:25 +0600

    update controller

--- src/main/java/com/shahariyr/mvc/controller/MainController.java
+++ src/main/java/com/shahariyr/mvc/controller/MainController.java
@@ -3,15 +3,12 @@ package com.shahariyr.mvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ch.qos.logback.core.model.Model;
-
 @Controller
 public class MainController {
 
 	@GetMapping("/hello")
-	public String sayhello(Model theModel) {
-		
-		theModel.addText("Shahariyr Reza");
-		return "helloworld";
+	public String sayhello() {
+
+		return "Index";
 	}
 }
