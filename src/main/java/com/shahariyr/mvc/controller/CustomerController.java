0e2a190220eadbdadf78060e4cec2ca6154e948c
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-11-28 13:14:57 +0600

    update controller
    add postmapping

--- src/main/java/com/shahariyr/mvc/controller/CustomerController.java
+++ src/main/java/com/shahariyr/mvc/controller/CustomerController.java
@@ -2,8 +2,14 @@ package com.shahariyr.mvc.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.shahariyr.mvc.entity.customer;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CustomerController {
@@ -11,10 +17,20 @@ public class CustomerController {
 	@GetMapping("/")
 	public String showform(Model theModel) {
 
-		theModel.addAttribute("customer", new customer());
+		theModel.addAttribute("customer", new customer());//"customer" is the model name. 
+		//Which has to be same as the thymeleaf file action.
 
-		return "Customer-form";
+		return "customer-form";
+	}
+	
+	@PostMapping("/processForm")
+	public String processForm(@Valid @ModelAttribute("customer") customer theCustomer, BindingResult theBindingResult) {
+		
+		if (theBindingResult.hasErrors()) {
+			return "customer-form";
+		} 
+		else {
+			return "confirmation-page";
+		}
 	}
 }
-//"customer" is the model name. 
-//Which has to be same as the thymeleaf file action.
