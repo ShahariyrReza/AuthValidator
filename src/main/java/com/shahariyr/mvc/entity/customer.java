0b113052c0341fa49b9067d11b1098b127e24df3
Shahariyr reza <shahariyr.reza@hotmail.com>
2024-12-14 20:56:22 +0600

    update customer

--- src/main/java/com/shahariyr/mvc/entity/customer.java
+++ src/main/java/com/shahariyr/mvc/entity/customer.java
@@ -7,10 +7,12 @@ import jakarta.validation.constraints.Size;
 
 public class customer {
 
-	
-	/* Here four field defined for login page
-	 * userName, email, monileNumber, password */
-	
+	/*
+	 * Here four field defined for login page userName, email, monileNumber,
+	 * password
+	 * 
+	 */
+
 	@NotNull(message = "required User Name")
 	private String userName;
 
@@ -18,22 +20,23 @@ public class customer {
 	@Size(min = 1, message = "required")
 	@Email
 	private String email;
-	
-	@Size(min = 11,max = 14, message = "use a valid phone number")
+
+	@Size(min = 11, max = 14, message = "use a valid phone number")
 	@Pattern(regexp = "^[+\\d]+$", message = "use a valid phone number")
 	private String mobileNumber;
 
 	@NotNull(message = "Required Password")
-	//@NotBlank(message = "Required Password")
+	// @NotBlank(message = "Required Password")
 	@Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
-	//@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d){8,}$", 
-    //message = "Password must have at least one upper case letter, one lower case letter, one number, and one special character")
+	// @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d){8,}$",
+	// message = "Password must have at least one upper case letter, one lower case
+	// letter, one number, and one special character")
 	private String password;
 	
 	
 
-	/* Getter Setter method correspondingly*/
-	
+	/* Getter Setter method correspondingly */
+
 	public String getUserName() {
 		return userName;
 	}
@@ -41,7 +44,6 @@ public class customer {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
 
 	public String getEmail() {
 		return email;
@@ -50,7 +52,6 @@ public class customer {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
 
 	public String getMobileNumber() {
 		return mobileNumber;
